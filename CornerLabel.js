bb7238a3e238341f0c7152fd43c7fa1fabd6ff6a
cyqresig <moonsunfall@aliyun.com>
2016-07-29 10:23:49 +0800

    added new feature: the position of corner label can be left or right

--- CornerLabel.js
+++ CornerLabel.js
@@ -6,19 +6,19 @@
  */
 
 import React, {
-  PropTypes,
-  Component,
+    PropTypes,
+    Component,
 } from 'react'
 import {
-  StyleSheet,
-  View,
-  Text,
+    StyleSheet,
+    View,
+    Text,
 } from 'react-native'
 
 const styles = StyleSheet.create({
     container: {
         position: 'absolute',
-        transform: [{rotate: '45deg'}],
+        //transform: [{rotate: '45deg'}],
         justifyContent: 'flex-end',
     },
     label: {
@@ -34,47 +34,66 @@ const styles = StyleSheet.create({
 
 export default class CornerLabel extends Component {
 
-  static PropTypes = {
-      style: View.propTypes.style,
-      textStyle: Text.propTypes.style,
-      cornerRadius: PropTypes.number.isRequired,
-  }
-
-  // 构造
-    constructor(props) {
-      super(props)
-      // 初始状态
-      this.state = {}
-
-      this._labelHeight = Math.sqrt( Math.pow(props.cornerRadius, 2) / 2 )
-      this._labelWidth = this._labelHeight * 2
-      let originOffset = Math.sqrt( Math.pow(this._labelHeight / 2, 2) / 2)
-      this._labelRight =  - this._labelWidth / 2 + originOffset
-      this._labelTop = - this._labelHeight / 2 +  originOffset
+    static defaultProps = {
+        alignment: 'left',
     }
 
-  render() {
-     return (
-        <View style={[styles.container,
-                      {width: this._labelWidth, height: this._labelHeight, right: this._labelRight, top: this._labelTop,},
+    static PropTypes = {
+        style: View.propTypes.style,
+        textStyle: Text.propTypes.style,
+        cornerRadius: PropTypes.number.isRequired,
+        alignment: PropTypes.oneOf([
+            'left',
+            'right',
+        ])
+    }
+
+    // 构造
+    constructor (props) {
+        super(props)
+        // 初始状态
+        this.state = {}
+
+        this._labelHeight = Math.sqrt(Math.pow(props.cornerRadius, 2) / 2)
+        this._labelWidth = this._labelHeight * 2
+        let originOffset = Math.sqrt(Math.pow(this._labelHeight / 2, 2) / 2)
+        let labelHorizontalPosition = -this._labelWidth / 2 + originOffset
+        let labelVerticalPosition = - this._labelHeight / 2 +  originOffset
+        if(props.alignment == 'left') {
+            this._labelPosition = {left : labelHorizontalPosition, top: labelVerticalPosition}
+            this._labelTransform = {transform: [{rotate: '-45deg'}]}
+        }
+        else {
+            this._labelPosition = {right : labelHorizontalPosition, top: labelVerticalPosition}
+            this._labelTransform = {transform: [{rotate: '45deg'}]}
+        }
+
+    }
+
+    render () {
+        return (
+            <View style={[styles.container,
+                      this._labelPosition,
+                      this._labelTransform,
+                      {width: this._labelWidth, height: this._labelHeight, },
                        ]}>
-          <View style={[styles.label,
+                <View style={[styles.label,
                         {height: this._labelHeight},
                         this.props.style,
                         ]}>
-              {this._renderChildren()}
-          </View>
-        </View>
-     )
-  }
+                    {this._renderChildren()}
+                </View>
+            </View>
+        )
+    }
 
-  _renderChildren() {
-     return React.Children.map(this.props.children, (child) => {
-        if(!React.isValidElement(child)) {
-            return <Text style={[styles.text, this.props.textStyle]}>{child}</Text>
-        }
-        return child
-     })
-  }
+    _renderChildren () {
+        return React.Children.map(this.props.children, (child) => {
+            if (!React.isValidElement(child)) {
+                return <Text style={[styles.text, this.props.textStyle]}>{child}</Text>
+            }
+            return child
+        })
+    }
 
 }
