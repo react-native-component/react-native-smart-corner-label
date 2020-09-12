f5c4ce6be3deb15608c655f5bd97b42ec3ad01b1
Usamaliaquat123 <inconnent12345@outlook.com>
2020-09-12 12:51:34 +0500

    asdsada

--- CornerLabel.js
+++ CornerLabel.js
@@ -1,9 +1,3 @@
-/*
- * A smart corner-label for react-native apps
- * https://github.com/react-native-component/react-native-smart-sudoku-grid/
- * Released under the MIT license
- * Copyright (c) 2016 react-native-component <moonsunfall@aliyun.com>
- */
 
 import React, {
     PropTypes,
